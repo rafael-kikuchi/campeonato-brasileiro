a16036a7453f0c0707b7e222b4810a25562afade
Rafael Kikuchi <rafael.kikuchi1991@gmail.com>
2021-10-02 00:57:35 -0300

    Layout Inicial e Definição de Menus

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,71 @@
 <template>
   <v-app>
+
+    <!--  *** App-Bar ***  -->
+
     <v-app-bar
+      color="green lighten-1"
       app
-      color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-toolbar-title>Campeonato Brasileiro ⚽</v-toolbar-title>
     </v-app-bar>
 
+    <!--  *** Menu Gaveta ***  -->
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="green lighten-1--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Início</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-clipboard-list</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Classificação</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-main>
-      <HelloWorld/>
+      <v-container>
+        <Inicio />
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Inicio from './components/Inicio.vue'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Inicio
   },
 
   data: () => ({
-    //
+    drawer: false,
+    group: null,
   }),
 };
 </script>
